b489a09a8747986808ed0511395d7f7d2d1b946a
Azalea Davies <azal34000@gmail.com>
2025-10-17 10:59:00 +0100

    Delete data directory

--- data/actual code/studentthing.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd, matplotlib.pyplot as plt
-
-df = pd.DataFrame(pd.read_csv("files to read\\StudentPerformanceFactors.csv"))
-plt.scatter(df["Distance_from_Home"], df["Attendance"])
-plt.show()
